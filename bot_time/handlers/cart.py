981c9a1ec7944f37a56c2b9fe9719ad58f61bb81
andrt <andr.toshachakov@gmail.com>
2021-08-03 18:14:38 +0300

    добавил файл-пасхалка cart

--- bot_time/handlers/cart.py
+++ bot_time/handlers/cart.py
@@ -1,9 +1,10 @@
 from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters import Text
 
 
-async def lesson_start(message: types.Message):
-    await message.answer()
+async def cart_func(message: types.Message):
+    await message.answer('ТАНЯ УЙДИТЕ')
 
 
-def register_handlers_lesson(dp: Dispatcher):
-    dp.register_message_handler(lesson_start, commands='lesson', state="*")
+def register_handlers_cart(dp: Dispatcher):
+    dp.register_message_handler(cart_func, Text(equals=".-."), state="*")
--- bot_time/handlers/register_cmd.py
+++ bot_time/handlers/register_cmd.py
@@ -1,3 +1,4 @@
+from bot_time.handlers.cart import register_handlers_cart
 from bot_time.handlers.cmd import register_handlers_start
 from bot_time.handlers.lessons import register_handlers_lesson
 from bot_time.handlers.time import register_handlers_time
@@ -5,5 +6,6 @@ from bot_time.handlers.time import register_handlers_time
 
 def reg_cmd(dp):
     register_handlers_start(dp)
+    register_handlers_cart(dp)
     register_handlers_lesson(dp)
     register_handlers_time(dp)
