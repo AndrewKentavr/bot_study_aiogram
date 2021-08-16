961afad34c8fdef35afa226ffd1fcbaebb351b87
andrt <andr.toshachakov@gmail.com>
2021-08-16 16:04:47 +0300

    I created the first normal parser on the site 'https://www.problems.ru'

--- pdf_parsing/print_text_by_page.py
+++ pdf_parsing/print_text_by_page.py
@@ -2,9 +2,10 @@ from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 
 path = 'C:/Users/andrt/PycharmProjects/bot_study_aiogram/pdf_parsing/Evgeni_Onegin.pdf'
+path_math = 'C:/Users/andrt/Desktop/Обушение/Матеша/своё/сборник задач по математике сканави.pdf'
 
 count = 0
-for page_layout in extract_pages(path, page_numbers=[3, 7]):
+for page_layout in extract_pages(path_math, page_numbers=[3, 7]):
     for element in page_layout:
         if isinstance(element, LTTextContainer):
             print(element.get_text())
